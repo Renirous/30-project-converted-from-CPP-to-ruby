01d73040d14d085a5e325985a8193e74316cd8b0
Renirous <ivanplasun@gmail.com>
2024-03-15 13:04:24 -0400

    modify_2

--- solve_null_file/project_23.rb
+++ solve_null_file/project_23.rb
@@ -1,5 +1,5 @@
 # Write a program that accept from the user a list of positive integer numbers which
-# terminate when teh user enters -1 the program should calculate how many numbers
+# terminate when the user enters -1 the program should calculate how many numbers
 # are entered from the range 1 to 10, 11 to 20, 21 to 30, 31 to 40 or 41 to 50.
 
 
