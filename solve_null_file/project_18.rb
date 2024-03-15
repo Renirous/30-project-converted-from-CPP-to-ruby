078690dbfe8544c224b37f266a1c7976cd8b14d7
Renirous <ivanplasun@gmail.com>
2024-03-15 13:04:22 -0400

    modify

--- solve_null_file/project_18.rb
+++ solve_null_file/project_18.rb
@@ -1,5 +1,5 @@
 # Write a program to calculate the area of a square [area = side^2] or a triangle [area = (base*height)/2]
-# the program accept from the user the type of shape as a character (t oos s) and output the area.
+# the program accept from the user the type of shape as a character (t or s) and output the area.
 
 # Way_Two
 
